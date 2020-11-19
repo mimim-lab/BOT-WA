04fdbbf35c2520f7262844cb5fcc3646a67f2bd6
mimim-lab <74508231+mimim-lab@users.noreply.github.com>
2020-11-19 18:58:10 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ const tambahan = require("./lib/tambahan.js")
 const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
-const BotName = 'BOT-WA'; 
-const instagram = 'https://www.instagram.com/kreasi.editing_official/'; 
-const telegram = 'https://t.me/MiMiMBOTWA'; 
+const BotName = 'NAMA BOTMU'; 
+const instagram = 'LINK INSTAGRAMU'; 
+const telegram = 'LINK TELEMU'; 
 const kapanbotaktif = 'KETIKA HANYA DIAKTIFKAN'; 
-const youtube = 'ORA NDUE SU';
-//const grupch1 = 'https://chat.whatsapp.com/DSId5QFJwHT9M6sFRMEKuq'; 
-//const grupch2 = 'https://chat.whatsapp.com/I0Gkm2iSNp22CtXAhMqplW'; 
+const youtube = 'LINK YTMU';
+//const grupch1 = 'belum ada grup'; 
+//const grupch2 = 'belum ada grup' ; 
 const
 {
    WAConnection,
@@ -49,7 +49,7 @@ conn.on('qr', qr =>
    {
       small: true
    });
-   console.log(`[ ${moment().format("HH:mm:ss")} ] Scan kode qr mu su!`);
+   console.log(`[ ${moment().format("HH:mm:ss")} ] Scan kode qr dengan whatsapp!`);
 });
 
 conn.on('credentials-updated', () =>
@@ -68,7 +68,7 @@ conn.on('user-presence-update', json => console.log(json.id + ' presence is => '
 conn.on('message-status-update', json =>
 {
    const participant = json.participant ? ' (' + json.participant + ')' : '' // participant exists when the message is from a group
-   console.log(`[ ${moment().format("HH:mm:ss")} ] => bot by ig:@kreasi.editing_official`)
+   console.log(`[ ${moment().format("HH:mm:ss")} ] => bot by ig:@reuz.id`)
 })
 
 conn.on('message-new', async(m) =>
