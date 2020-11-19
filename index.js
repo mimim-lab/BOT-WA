b67150905ef3da558e44ef88122db9f0c101625d
mimim-lab <74508231+mimim-lab@users.noreply.github.com>
2020-11-19 19:58:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ const tambahan = require("./lib/tambahan.js")
 const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
-const BotName = 'NAMA BOTMU'; 
-const instagram = 'LINK INSTAGRAMU'; 
-const telegram = 'LINK TELEMU'; 
+const BotName = 'BOT-WA'; 
+const instagram = 'https://www.instagram.com/kreasi.editing_official/'; 
+const telegram = 'https://t.me/MiMiMBOTWA'; 
 const kapanbotaktif = 'KETIKA HANYA DIAKTIFKAN'; 
-const youtube = 'LINK YTMU';
-//const grupch1 = 'belum ada grup'; 
-//const grupch2 = 'belum ada grup' ; 
+const youtube = 'RAK NDUE YUTUB COK';
+//const grupch1 = 'https://chat.whatsapp.com/DSId5QFJwHT9M6sFRMEKuq'; 
+//const grupch2 = 'https://chat.whatsapp.com/I0Gkm2iSNp22CtXAhMqplW' ; 
 const
 {
    WAConnection,
@@ -68,7 +68,7 @@ conn.on('user-presence-update', json => console.log(json.id + ' presence is => '
 conn.on('message-status-update', json =>
 {
    const participant = json.participant ? ' (' + json.participant + ')' : '' // participant exists when the message is from a group
-   console.log(`[ ${moment().format("HH:mm:ss")} ] => bot by ig:@reuz.id`)
+   console.log(`[ ${moment().format("HH:mm:ss")} ] => bot by ig:@kreasi.editing_official`)
 })
 
 conn.on('message-new', async(m) =>
